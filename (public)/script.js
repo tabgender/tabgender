8e62815b23941ac8d49d50dfaa6ae07da32d77a2
ShivumB <shivumbanerjee24@gmail.com>
2023-05-26 23:23:12 -0500

    Fixed scrolling

--- (public)/script.js
+++ (public)/script.js
@@ -23,12 +23,86 @@ function update(time, date) {
 update(time, date);
 setInterval(update, 750, time, date);
 
-//toggle blur off by default
-document.getElementById("live-simply").classList.toggle("blur-[2px]");
-
 //toggle options
-function toggleOptions() {
+document.getElementById("options-page").classList.toggle("visible");
 
-  document.getElementById("live-simply").classList.toggle("blur-[2px]");
-  document.getElementById("options-page").classList.toggle("invisible");
+function toggleOptions() {
+  document.getElementById("options-page").classList.toggle("visible");
+  document.getElementById("live-simply").classList.toggle("brightness-[0.7]");
 }
+
+//toggle content in options page
+function contentChange(e){
+  if (e == 0){
+    document.getElementById("options-header").innerHTML = "Background";
+  } else if (e == 1){
+    document.getElementById("options-header").innerHTML = "Shortcuts";
+  } else if (e == 2){
+    document.getElementById("options-header").innerHTML = "Cards";
+  } else if (e == 3){
+    document.getElementById("options-header").innerHTML = "Color & Theme";
+  }
+
+}
+var currentBackground1 = 'from-blue-300';
+var currentBackground2 = 'to-pink-400';
+
+function backgroundChange(j){
+  if(j == 0){
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#f80c16]';
+    currentBackground2 = 'to-[#e4bec0]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+  else if(j==1){
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#103cf3]';
+    currentBackground2 = 'to-[#a8b5ed]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+  else if(j==2){
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#28fa43]';
+    currentBackground2 = 'to-[#8ef59b]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+  else if(j==3){
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#d7f420]';
+    currentBackground2 = 'to-[#e5f19a]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+  else if(j==4){
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#d3b3ec]';
+    currentBackground2 = 'to-[#500091]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+  else{
+    const liveSimplyEl = document.getElementById("live-simply");
+    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
+    currentBackground1 = 'from-[#f522e7]';
+    currentBackground2 = 'to-[#f18feb]';
+    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
+  }
+
+}
+
+const color1Input = document.getElementById('color1');
+const color2Input = document.getElementById('color2');
+const liveSimplyEl = document.getElementById("live-simply");
+
+function updateGradient() {
+  const color1 = color1Input.value;
+  const color2 = color2Input.value;
+  
+  liveSimplyEl.style.backgroundImage = `linear-gradient(to bottom right, ${color1}, ${color2})`;
+}
+
+color1Input.addEventListener('input', updateGradient);
+color2Input.addEventListener('input', updateGradient);
