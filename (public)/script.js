74bf03f50036ed36ff458b3632a713fc602d14a1
ShivumB <112597214+tabgender@users.noreply.github.com>
2023-07-04 20:02:40 -0500

    Updated site

--- (public)/script.js
+++ (public)/script.js
@@ -11,98 +11,102 @@ function update(time, date) {
     time.textContent = (d.getHours() - 12) + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " pm";
   } else if (d.getHours() == 12) {
     time.textContent = 12 + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " pm";
-  } else if(d.getHours() == 0){
+  } else if (d.getHours() == 0) {
     time.textContent = 12 + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " am";
   } else {
     time.textContent = d.getHours() + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " am";
   }
 
-  date.textContent = daysOfWeek[d.getDay()] + ", " + monthsOfYear[d.getMonth()] + " " + d.getDate();
+  // date.textContent = daysOfWeek[d.getDay()] + ", " + monthsOfYear[d.getMonth()] + " " + d.getDate();
 }
 
 update(time, date);
 setInterval(update, 750, time, date);
 
 //toggle options
-document.getElementById("options-page").classList.toggle("visible");
+var hideOptions = true;
+const optionsPage = document.getElementById("options-page");
+const settings = document.getElementById("settings");
 
 function toggleOptions() {
-  document.getElementById("options-page").classList.toggle("visible");
-  document.getElementById("live-simply").classList.toggle("brightness-[0.7]");
+  optionsPage.style.transition = "opacity 0.2s";
+
+  hideOptions = !hideOptions;
+
+  optionsPage.style.opacity = (hideOptions) ? "0" : "1";
+  optionsPage.style.pointerEvents = (hideOptions) ? "none" : "all";
 }
 
-//toggle content in options page
-function contentChange(e){
-  if (e == 0){
-    document.getElementById("options-header").innerHTML = "Background";
-  } else if (e == 1){
-    document.getElementById("options-header").innerHTML = "Shortcuts";
-  } else if (e == 2){
-    document.getElementById("options-header").innerHTML = "Cards";
-  } else if (e == 3){
-    document.getElementById("options-header").innerHTML = "Color & Theme";
-  }
+//toggle customization
+var hideCustomization = true;
+const customizationPage = document.getElementById("customization-page");
+const customizationButton = document.getElementById("customization-button");
 
-}
-var currentBackground1 = 'from-blue-300';
-var currentBackground2 = 'to-pink-400';
+function toggleCustomization() {
 
-function backgroundChange(j){
-  if(j == 0){
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#f80c16]';
-    currentBackground2 = 'to-[#e4bec0]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
-  else if(j==1){
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#103cf3]';
-    currentBackground2 = 'to-[#a8b5ed]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
-  else if(j==2){
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#28fa43]';
-    currentBackground2 = 'to-[#8ef59b]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
-  else if(j==3){
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#d7f420]';
-    currentBackground2 = 'to-[#e5f19a]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
-  else if(j==4){
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#d3b3ec]';
-    currentBackground2 = 'to-[#500091]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
-  else{
-    const liveSimplyEl = document.getElementById("live-simply");
-    liveSimplyEl.classList.remove(currentBackground1, currentBackground2);
-    currentBackground1 = 'from-[#f522e7]';
-    currentBackground2 = 'to-[#f18feb]';
-    liveSimplyEl.classList.add(currentBackground1, currentBackground2);
-  }
+  if (!hideOptions) toggleOptions();
 
+  customizationPage.style.transition = "opacity 0.2s";
+
+  hideCustomization = !hideCustomization;
+
+  customizationPage.style.opacity = (hideCustomization) ? "0" : "1";
+  customizationPage.style.pointerEvents = (hideCustomization) ? "none" : "all";
 }
 
-const color1Input = document.getElementById('color1');
-const color2Input = document.getElementById('color2');
-const liveSimplyEl = document.getElementById("live-simply");
+// Event listener to close the options page when clicking outside
+document.addEventListener('click', (event) => {
+  const targetElement = event.target;
 
-function updateGradient() {
-  const color1 = color1Input.value;
-  const color2 = color2Input.value;
+  // Check if the clicked element is the options button or inside the options page
+  if (!hideOptions && targetElement !== settings && !optionsPage.contains(targetElement)) {
+    toggleOptions();
+  }
+
+  //check if the clicked element is inside the customization page
+  if (!hideCustomization && !optionsPage.contains(targetElement) && !customizationPage.contains(targetElement)) {
+    toggleCustomization();
+  }
+});
+
+//store customization
+const main = document.getElementById("main");
+
+var currentButton = "color0";
+var currentBackground = "linear-gradient(#93C5FD, #F472B6)";
+
+var storedButton = JSON.parse(window.localStorage.getItem("button"));
+if(storedButton != null) currentButton = storedButton;
+
+var storedBackground = JSON.parse(window.localStorage.getItem("background"));
+if(storedBackground != null) currentBackground = storedBackground;
+
+//give initial styling
+var currentButtonElement = document.getElementById(currentButton);
+currentButtonElement.classList.add("border");
+currentButtonElement.classList.add("border-2");
+currentButtonElement.classList.add("border-black");
+
+changeBackground(currentBackground, currentButton);
+
+function changeBackground(newBackground, newButton) {
+
+  currentBackground = newBackground;
+  main.style.background = currentBackground;
+  main.style.backgroundSize = "100%";
+
+  currentButtonElement = document.getElementById(currentButton);
+  currentButtonElement.classList.remove("border");
+  currentButtonElement.classList.remove("border-2");
+  currentButtonElement.classList.remove("border-black");
+
+  currentButton = newButton;
   
-  liveSimplyEl.style.backgroundImage = `linear-gradient(to bottom right, ${color1}, ${color2})`;
-}
+  currentButtonElement = document.getElementById(currentButton);
+  currentButtonElement.classList.add("border");
+  currentButtonElement.classList.add("border-2");
+  currentButtonElement.classList.add("border-black");
 
-color1Input.addEventListener('input', updateGradient);
-color2Input.addEventListener('input', updateGradient);
+  window.localStorage.setItem("button", JSON.stringify(currentButton));
+  window.localStorage.setItem("background", JSON.stringify(currentBackground));
+}
